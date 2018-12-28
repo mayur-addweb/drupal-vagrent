62677017f24e6018aca761e22e23f3a1c1809488
jigish-addweb <jigish.addweb@gmail.com>
2018-12-28 15:05:27 +0530

    Add the id spell mistake of HTML

--- frontapp/sdcom-react/src/screens/dashboard/Header/Header.js
+++ frontapp/sdcom-react/src/screens/dashboard/Header/Header.js
@@ -14,7 +14,7 @@ class Header extends Component {
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ml-auto topnav">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#industirs">INDUSTRIES <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="#industries">INDUSTRIES <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#portfolio">PORTFOLIO</a>
