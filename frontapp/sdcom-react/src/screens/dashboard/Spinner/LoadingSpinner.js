0706d264df3389ae5dd08dffc060dff24c770af4
jigish-addweb <jigish.addweb@gmail.com>
2018-12-28 15:40:05 +0530

    Change content Loading

--- frontapp/sdcom-react/src/screens/dashboard/Spinner/LoadingSpinner.js
+++ frontapp/sdcom-react/src/screens/dashboard/Spinner/LoadingSpinner.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LoadingSpinner = () => (
   <div className="sdcom-spinner">
-    <i className=" spinner fa fa-spinner fa-spin" /> <h2 class="spinner-text">Loading...</h2>
+    <i className=" spinner fa fa-spinner fa-spin" /> <h2 class="spinner-text">Submitting...</h2>
   </div>
 );
 
